2e6b82302cd55ec7fabaf460b974d16eb6378e4b
Simo Jokela <simo.jokela@nordsoftware.com>
2016-02-25 11:20:30 +0200

    Add push notification support and also add background geolocation library

--- ios/OpenCity/AppDelegate.m
+++ ios/OpenCity/AppDelegate.m
@@ -11,7 +11,25 @@
 
 #import "RCTRootView.h"
 
+#import "RCTPushNotificationManager.h"
+
 @implementation AppDelegate
+
+// Required to register for notifications
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+  [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings];
+}
+// Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+// Required for the notification event.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
+{
+  [RCTPushNotificationManager didReceiveRemoteNotification:notification];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
