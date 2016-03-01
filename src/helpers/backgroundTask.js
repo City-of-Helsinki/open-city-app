46fafd8b2900cb0113f6cf2f6439ff6715318937
Simo Jokela <simo.jokela@nordsoftware.com>
2016-03-01 13:33:26 +0200

    Change some background geolocation configurations

--- src/helpers/backgroundTask.js
+++ src/helpers/backgroundTask.js
@@ -114,12 +114,11 @@ export function mountBackgroundTask(navigator) {
   BackgroundGeolocation.configure({
     desiredAccuracy: 0,
     stationaryRadius: 50,
-    distanceFilter: 50,
-    disableElasticity: false, // <-- [iOS] Default is 'false'.  Set true to disable speed-based distanceFilter elasticity
+    distanceFilter: 500,
     locationUpdateInterval: 5000,
     minimumActivityRecognitionConfidence: 80,   // 0-100%.  Minimum activity-confidence for a state-change
-    fastestLocationUpdateInterval: 10000,
-    activityRecognitionInterval: 10000,
+    fastestLocationUpdateInterval: 5000,
+    activityRecognitionInterval: 5000,
     stopDetectionDelay: 1,  // <--  minutes to delay after motion stops before engaging stop-detection system
     stopTimeout: 2, // 2 minutes
     activityType: 'AutomotiveNavigation',
@@ -129,8 +128,8 @@ export function mountBackgroundTask(navigator) {
     forceReloadOnLocationChange: false,  // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a new location is recorded (WARNING: possibly distruptive to user)
     forceReloadOnMotionChange: false,    // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when device changes stationary-state (stationary->moving or vice-versa) --WARNING: possibly distruptive to user)
     forceReloadOnGeofence: false,        // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a geofence crossing occurs --WARNING: possibly distruptive to user)
-    stopOnTerminate: false,              // <-- [Android] Allow the background-service to run headless when user closes the app.
-    startOnBoot: true                   // <-- [Android] Auto start background-service in headless mode when device is powered-up.
+    stopOnTerminate: true,              // <-- [Android] Allow the background-service to run headless when user closes the app.
+    startOnBoot: false                   // <-- [Android] Auto start background-service in headless mode when device is powered-up.
   });
 
   // This handler fires whenever bgGeo receives a location update.
