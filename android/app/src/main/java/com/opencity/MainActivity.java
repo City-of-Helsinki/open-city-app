fe75ee49ad8333902583bd1c980dd629afe82e1b
Simo Jokela <simo.jokela@nordsoftware.com>
2016-04-13 16:42:32 +0300

    Add image picker module

--- android/app/src/main/java/com/opencity/MainActivity.java
+++ android/app/src/main/java/com/opencity/MainActivity.java
@@ -2,6 +2,7 @@ package com.opencity;
 
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactActivity;
+import com.imagepicker.ImagePickerPackage;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -38,7 +39,8 @@ public class MainActivity extends ReactActivity {
         return Arrays.<ReactPackage>asList(
         new AirPackage(),
                 new ReactNativeLocalizationPackage()
-            new MainReactPackage()
+            new MainReactPackage(),
+        new ImagePickerPackage()
         );
     }
 }
