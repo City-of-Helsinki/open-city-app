43054390ba0f8ef076facf6fd142f8a1806f34a8
Toni <toni.nahkala@haltu.fi>
2016-09-16 10:21:04 +0300

    Add Navbar file

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+class Navbar extends Component {
+
+  constructor(props, context) {
+    super(props);
+  }
+
+  render() {
+
+    // Add Padding for iOS status bar
+    var iosStatusBar = null;
+    if (Platform.OS !== 'android') {
+      iosStatusBar = <View style={styles.iosStatusBar}></View>
+    }
+
+    return (
+      <View>
+        {iosStatusBar}
+        <View style={styles.container}>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 55,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: '#F15A24',
+  },
+  iosStatusBar: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    height: 15,
+    backgroundColor: '#F15A24',
+  },
+});
+
+
+module.exports = Navbar
