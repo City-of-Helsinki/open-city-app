339f288ff699d071188cbaf012ff2a06d43ff9fa
Toni <toni.nahkala@haltu.fi>
2016-09-28 10:49:20 +0300

    Add list translation file, issuelist changes

---
+++ src/translations/list.js
@@ -0,0 +1,10 @@
+import LocalizedStrings from 'react-native-localization';
+
+export default new LocalizedStrings({
+  en:{
+    listViewTitle: 'List',
+  },
+  fi: {
+    listViewTitle: 'Palaute',
+  }
+});
--- src/views/IssueListView.js
+++ src/views/IssueListView.js
@@ -13,6 +13,7 @@ import Drawer      from 'react-native-drawer'
 import Navbar      from './../components/Navbar';
 import Menu        from './../components/Menu';
 import makeRequest from './../util/requests';
+import Util        from './../util/util';
 import Config      from './../config';
 
 // Translations
@@ -37,12 +38,12 @@ class IssueListView extends Component {
   }
 
   fetchIssues() {
-    var url = Config.OPEN311_SERVICE_REQUESTS_URL + '?start_date=2016-08-24T00:00:00Z&end_date=2016-09-24T00:00:00Z';
+    var url = Config.OPEN311_SERVICE_REQUESTS_URL + '?start_date=2016-08-24T00:00:00Z&end_date=2016-09-24T00:00:00Z&extensions=true';
     var headers = {'Accept': 'application/json', 'Content-Type': 'application/json'};
 
     makeRequest(url, 'GET', headers, null)
     .then(result => {
-
+      Util.parseIssueList(result);
     }, err => {
       showAlert(transError.networkErrorTitle, transError.networkErrorMessage, transError.networkErrorButton);
     });
