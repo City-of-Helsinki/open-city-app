88b1affcee7a6c382f405fcaffc3dc9da1a9d934
Toni <toni.nahkala@haltu.fi>
2016-10-11 16:35:43 +0300

    Add comments, fix class name

--- src/views/IntroductionView.js
+++ src/views/IntroductionView.js
@@ -9,22 +9,18 @@ import {
   LayoutAnimation
 } from 'react-native';
 
-import splashImage from './../img/splash_image.png';
-
 import transIntroduction from '../translations/introduction';
 
 import showAlert   from '../components/Alert';
-import Spinner     from '../components/Spinner';
-import makeRequest from '../util/requests';
 import Util        from '../util/util';
-import Models      from '../util/models';
 import Config      from '../config';
 
-const EVENT_MOVE_RIGHT_THRESHOLD = -30;
-const EVENT_MOVE_LEFT_THRESHOLD = 30;
+const EVENT_RIGHT_SWIPE_THRESHOLD = -30;
+const EVENT_LEFT_SWIPE_THRESHOLD  = 30;
 
-// SplashScreen shown while data is being loaded
-class SplashScreen extends Component {
+// An introductory view where user can swipe to move back and forth. After the final view user is
+// redirected to the MainView. The view is divided into 3 seperate states.
+class IntroductionView extends Component {
 
   constructor(props, context) {
     super(props, context);
@@ -32,38 +28,39 @@ class SplashScreen extends Component {
     transIntroduction.setLanguage('fi');
 
     this.state = {
-      topText: transIntroduction.firstTopText,
-      bottomText: transIntroduction.firstBottomText,
-      image: '',
-      progressImage: '',
-      currentView: 1,
+      topText: transIntroduction.firstTopText,        // The text displayd on the top
+      bottomText: transIntroduction.firstBottomText,  // The text displayd on the bottom
+      image: '',                                      // An image which is located on the middle
+      progressImage: '',                              // Image which displays users progress in the introduction view
+      currentView: 1,                                 // Index of the current view of the introductory flow
     };
   }
 
   componentWillMount() {
-    console.log('xxx')
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
       onPanResponderGrant: (evt, gestureState) => {},
-      onPanResponderMove: (evt, gestureState) => {
-        console.log('xxx')
-      },
+      onPanResponderMove: (evt, gestureState) => {},
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
-        if (gestureState.dx < EVENT_MOVE_RIGHT_THRESHOLD) {
+
+        // Set up the next introduction view, go to the MainView after the third introduction view
+        if (gestureState.dx < EVENT_RIGHT_SWIPE_THRESHOLD) {
           LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
           this.setState({
             topText: transIntroduction.thirdTopText,
             bottomText: transIntroduction.thirdBottomText,
           });
-        } else {
-          if (gestureState.dx > EVENT_MOVE_LEFT_THRESHOLD) {
-            this.navToMainView();
-          }
         }
+
+        // Go to the previous introduction view unless it's the first view in which case nothing will be done
+        else if (gestureState.dx > EVENT_LEFT_SWIPE_THRESHOLD) {
+          this.navToMainView();
+        }
+
       },
       onPanResponderTerminate: (evt, gestureState) => {},
       onShouldBlockNativeResponder: (evt, gestureState) => {return true;},
@@ -136,4 +133,4 @@ const styles = StyleSheet.create({
 
 });
 
-module.exports = SplashScreen
+module.exports = IntroductionView
