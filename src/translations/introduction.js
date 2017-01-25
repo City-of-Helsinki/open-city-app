21f22d2d061d4fdd315876ff817044148d85fb2e
Toni <toni.nahkala@haltu.fi>
2017-01-25 13:47:56 +0200

    Remove redundant translation

--- src/translations/introduction.js
+++ src/translations/introduction.js
@@ -2,14 +2,12 @@ import LocalizedStrings from 'react-native-localization';
 
 export default new LocalizedStrings({
   en: {
-    versionTitle: '',
     modalTitle: '',
     modalDescription: '',
     checkboxLabel: '',
     closeButton: 'CLOSE',
   },
   fi: {
-    versionTitle: '',
     modalTitle: 'Tervetuloa {0}-sovellukseen',
     modalDescription: 'Sovelluksella voit lähettää palautetta kaupungille ja selata kaupungin saamia palautteita.',
     checkboxLabel: 'Älä näytä tätä enää',
