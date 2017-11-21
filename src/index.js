b8a007d39cdfd3fca44c9fe544b4345a9c82a7f1
Sampo Laurila <sampo.laurila@siili.com>
2017-11-21 10:53:08 +0200

    chore (index) clean up unused code

--- src/index.js
+++ src/index.js
@@ -6,21 +6,8 @@ import {
   BackHandler
 } from 'react-native';
 
-// import {
-//   Navigator
-// } from 'react-native-deprecated-custom-components';
-
-import SplashScreen             from './views/SplashScreen';
-import MainView                 from './views/MainView';
-import SendServiceRequestView   from './views/SendServiceRequestView';
-import ServiceRequestListView   from './views/ServiceRequestListView';
-import IntroductionView         from './views/IntroductionView';
-import ServiceRequestDetailView from './views/ServiceRequestDetailView';
-import AppFeedbackView          from './views/AppFeedbackView';
-import ImageView                from './views/ImageView';
 import Global                   from './util/globals';
-
-import { Navigator } from './navigation';
+import { Navigator }            from './navigation';
 
 class OpenCity extends Component<{}> {
 
@@ -33,28 +20,6 @@ class OpenCity extends Component<{}> {
       <Navigator />
     );
   }
-
-  // navigatorRenderScene(route, navigator) {
-  //   _navigator = navigator;
-  //   switch (route.id) {
-  //     case 'SplashScreen':
-  //       return(<SplashScreen navigator={navigator} route={route} title='SplashScreen' />);
-  //     case 'IntroductionView':
-  //       return(<IntroductionView navigator={navigator} route={route} title='IntroductionView' />);
-  //     case 'MainView':
-  //       return(<MainView navigator={navigator} route={route} title='MainView' />);
-  //     case 'SendServiceRequestView':
-  //       return(<SendServiceRequestView navigator={navigator} route={route} title='SendServiceRequestView' />);
-  //     case 'ServiceRequestListView':
-  //       return(<ServiceRequestListView navigator={navigator} route={route} title='ServiceRequestListView' />);
-  //     case 'ServiceRequestDetailView':
-  //       return(<ServiceRequestDetailView navigator={navigator} route={route} title='ServiceRequestDetailView' />);
-  //     case 'AppFeedbackView':
-  //       return(<AppFeedbackView navigator={navigator} route={route} title='AppFeedbackView' />);
-  //     case 'ImageView':
-  //       return(<ImageView navigator={navigator} route={route} title='ImageView' />);
-  //   }
-  // }
 }
 
 BackHandler.addEventListener('hardwareBackPress', function() {
