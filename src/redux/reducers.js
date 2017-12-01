e1231daed1cf8cca88bc91f3f8b77be440e75d4d
Sampo Laurila <sampo.laurila@siili.com>
2017-12-01 09:29:29 +0200

    feat (hearings) set up redux and sagas

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as oidcReducer } from 'redux-oidc';
 import authReducer from './auth/reducer';
 import eventsReducer from './events/reducer';
+import hearingsReducer from './hearings/reducer';
 
 export default combineReducers({
   OIDC: oidcReducer,
   auth: authReducer,
-  events: eventsReducer
+  events: eventsReducer,
+  hearings: hearingsReducer
 });
