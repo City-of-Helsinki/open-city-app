c31510eb3a037e2d2a81b33326768957d2ce48a6
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-12-13 00:46:16 +0200

    Add brand icons, test on navbar

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {
   View,
-  ScrollView
+  ScrollView,
+  Image
 } from 'react-native';
 
 import { connect }            from 'react-redux';
 import { bindActionCreators } from 'redux';
 import EventActions           from '../../redux/events/actions';
 import HearingActions         from '../../redux/hearings/actions';
-
+ 
 import Navbar             from '../../components/Navbar';
 import Spinner            from '../../components/Spinner';
 import Hero               from '../../components/Hero';
@@ -26,6 +27,16 @@ class HomeView extends Component {
     this.props.eventActions.getHero();
     this.props.hearingActions.getHearings();
   }
+
+  static navigationOptions = {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./../../img/icon-home.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
 
   render() {
     const { heroEvent, hearingList } = this.props
